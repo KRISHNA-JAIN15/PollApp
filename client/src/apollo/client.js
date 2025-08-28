bb8e6af52bb11ea28722b08cc549b420e896014a
Krishna Jain <151719244+KRISHNA-JAIN15@users.noreply.github.com>
2025-08-29 00:09:58 +0530

    Update client.js

--- client/src/apollo/client.js
+++ client/src/apollo/client.js
@@ -1,9 +1,15 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
+// Use production URL directly
+const GRAPHQL_URI = "https://pollapp-ivzl.onrender.com/graphql";
+
 // Create HTTP link to GraphQL endpoint
 const httpLink = createHttpLink({
-  uri: "https://pollapp-ivzl.onrender.com/graphql",
+  uri: GRAPHQL_URI,
+  fetchOptions: {
+    timeout: 30000, // 30 second timeout for cold starts
+  },
 });
 
 // Create auth link to add JWT token to requests
@@ -21,6 +27,16 @@ const authLink = setContext((_, { headers }) => {
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      errorPolicy: "all",
+      fetchPolicy: "cache-and-network",
+    },
+    query: {
+      errorPolicy: "all",
+      fetchPolicy: "cache-first",
+    },
+  },
 });
 
 export default client;
